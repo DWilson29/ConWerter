bc281c989495aacecaae74a8e2e493ef1625337a
DWilson2 <deltadw8@gmail.com>
2026-02-17 20:48:31 -0800

    Change

--- main.go
+++ main.go
@@ -3,17 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
-	"time"
-
+  "time"
 	"github.com/gopxl/beep/v2"
 	"github.com/gopxl/beep/v2/generators"
 	"github.com/gopxl/beep/v2/speaker"
 )
 
 func main() {
-  f := 
-  1000
+  val := os.Args[1]
+  f := 1000.0
 	sr := beep.SampleRate(48000)
 	speaker.Init(sr, 4800)
 
@@ -23,24 +21,20 @@ func main() {
 	}
 
 	// Play 2 seconds of each tone
-	beep := sr.N(time.Second/10)
+	//short := sr.N(time.Second/10)
   long := sr.N(time.Second/2)
 
-	ch := make(chan struct{})
-	sounds := []beep.Streamer{
-		beep.Callback(print("sine")),
-		beep.Take(two, sine),
-		beep.Callback(func() {
-			ch <- struct{}{}
-		}),
-	}
-	speaker.Play(beep.Seq(sounds...))
-	<-ch
-}
-
-// a simple clousure to wrap fmt.Println
-func print(s string) func() {
-	return func() {
-		fmt.Println(s)
-	}
+  ch := make(chan struct{})
+  for index, s := range val {
+    fmt.Println(s)
+    fmt.Println(index)
+    sounds := []beep.Streamer{
+      beep.Take(long, sine),
+      beep.Callback(func() {
+        ch <- struct{}{}
+      }),
+    }
+	  speaker.Play(beep.Seq(sounds...))
+	  <-ch
+  }
 }
