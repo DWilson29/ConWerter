8f7dd7fae0965abfaeccf1a8d8299f3fef761a79
DWilson2 <deltadw8@gmail.com>
2026-03-01 15:41:04 -0800

    Update to use international morse code

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
   "time"
+  "strings"
 	"github.com/gopxl/beep/v2"
 	"github.com/gopxl/beep/v2/generators"
 	"github.com/gopxl/beep/v2/speaker"
@@ -12,7 +13,9 @@ import (
 
 func main() {
   val := os.Args[1]
-  f := 1000.0
+  lower := strings.ToLower(val)
+
+  f := 500.0
 	sr := beep.SampleRate(48000)
 	speaker.Init(sr, 4800)
 
@@ -25,41 +28,50 @@ func main() {
 		panic(err)
 	} 
 	// Play 2 seconds of each tone
-	short := sr.N(time.Second/10)
-  long := sr.N(time.Second/2)
-  longest := sr.N(time.Second)
+	short := sr.N(time.Second/9)
+  long := sr.N(time.Second/3)
 
   var morseCode = map[rune][]int{
     'a': {short, long},
     'b': {long, short, short, short},
-    'c': {short, short, short},
+    'c': {long, short, long, short},
     'd': {long, short, short},
     'e': {short},
-    'f': {short, long, short},
+    'f': {short, short, long, short},
     'g': {long, long, short},
     'h': {short, short, short, short},
     'i': {short, short},
-    'j': {long, short, long, short},
+    'j': {short, long, long ,long},
     'k': {long, short, long},
-    'l': {longest},
+    'l': {short, long, short, short},
     'm': {long, long},
     'n': {long, short},
-    'o': {short, short},
-    'p': {short, short, short, short, short},
-    'q': {short, short, long, short},
+    'o': {long, long, long},
+    'p': {short, long, long},
+    'q': {long, long, short, long},
     'r': {short, long, short},
     's': {short, short, short},
     't': {long},
     'u': {short, short, long},
     'v': {short, short, short, long},
     'w': {short, long, long},
-    'x': {short, long, short, short},
+    'x': {long, short, short, long},
     'y': {long, short, long, long},
     'z': {long, long, short, short},
+    '1': {short, long, long, long, long},
+    '2': {short, short, long, long, long},
+    '3': {short, short, short, long, long},
+    '4': {short, short, short, short, long},
+    '5': {short, short, short, short, short},
+    '6': {long, short, short, short, short},
+    '7': {long, long, short, short, short},
+    '8': {long, long, long, short, short},
+    '9': {long, long, long, long, short},
+    '0': {long, long, long, long, long},
   }
 
   ch := make(chan struct{})
-  for _, s := range val {
+  for _, s := range lower {
     morse := morseCode[s]
     var sounds []beep.Streamer
 
