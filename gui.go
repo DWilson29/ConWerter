4bbaaaba783b3c481bdb23b231fe4887cb124e50
DWilson2 <deltadw8@gmail.com>
2026-03-02 20:12:28 -0800

    expand sound playing

---
+++ gui.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "log"
+
+  "github.com/gotk3/gotk3/gtk"
+)
+
+func setup_window(title string) *gtk.Window {
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		log.Fatal("Unable to create window:", err)
+	}
+	win.SetTitle(title)
+	win.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+	win.SetDefaultSize(800, 600)
+	win.SetPosition(gtk.WIN_POS_CENTER)
+	return win
+}
+
+func setup_box(orient gtk.Orientation) *gtk.Box {
+	box, err := gtk.BoxNew(orient, 0)
+	if err != nil {
+		log.Fatal("Unable to create box:", err)
+	}
+	return box
+}
+
--- main.go
+++ main.go
@@ -1,90 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"os"
-  "time"
+  "os"
   "strings"
-	"github.com/gopxl/beep/v2"
-	"github.com/gopxl/beep/v2/generators"
-	"github.com/gopxl/beep/v2/speaker"
+  "github.com/gotk3/gotk3/gtk"
 )
 
 
 func main() {
-  val := os.Args[1]
-  lower := strings.ToLower(val)
+  gtk.Init(nil)
 
-  f := 500.0
-	sr := beep.SampleRate(48000)
-	speaker.Init(sr, 4800)
+  win := setup_window("ConWerter")
+  box := setup_box(gtk.ORIENTATION_VERTICAL)
 
-  sine, err := generators.SineTone(sr, f)
-	if err != nil {
-		panic(err)
-	} 
-	empty, err := generators.SineTone(sr, 0.0)
-	if err != nil {
-		panic(err)
-	} 
-	// Play 2 seconds of each tone
-	short := sr.N(time.Second/9)
-  long := sr.N(time.Second/3)
+  win.ShowAll()
 
-  var morseCode = map[rune][]int{
-    'a': {short, long},
-    'b': {long, short, short, short},
-    'c': {long, short, long, short},
-    'd': {long, short, short},
-    'e': {short},
-    'f': {short, short, long, short},
-    'g': {long, long, short},
-    'h': {short, short, short, short},
-    'i': {short, short},
-    'j': {short, long, long ,long},
-    'k': {long, short, long},
-    'l': {short, long, short, short},
-    'm': {long, long},
-    'n': {long, short},
-    'o': {long, long, long},
-    'p': {short, long, long},
-    'q': {long, long, short, long},
-    'r': {short, long, short},
-    's': {short, short, short},
-    't': {long},
-    'u': {short, short, long},
-    'v': {short, short, short, long},
-    'w': {short, long, long},
-    'x': {long, short, short, long},
-    'y': {long, short, long, long},
-    'z': {long, long, short, short},
-    '1': {short, long, long, long, long},
-    '2': {short, short, long, long, long},
-    '3': {short, short, short, long, long},
-    '4': {short, short, short, short, long},
-    '5': {short, short, short, short, short},
-    '6': {long, short, short, short, short},
-    '7': {long, long, short, short, short},
-    '8': {long, long, long, short, short},
-    '9': {long, long, long, long, short},
-    '0': {long, long, long, long, long},
-  }
+  gtk.Main()
 
-  ch := make(chan struct{})
-  for _, s := range lower {
-    morse := morseCode[s]
-    var sounds []beep.Streamer
+  //val := os.Args[1]
+  //lower := strings.ToLower(val)
 
-    for _, delay := range morse {
-      fmt.Println(delay)
-      sounds = append(sounds, beep.Take(delay, sine))
-      sounds = append(sounds, beep.Take(short, empty))
-    }
-
-    sounds = append(sounds, beep.Callback(func(){
-      ch <- struct{}{}
-    }))
-	  speaker.Play(beep.Seq(sounds...))
-	  <-ch
-  }
+  //playSound(lower)
 }
