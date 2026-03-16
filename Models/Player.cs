c80d88444960e6a01aad128cd3c9186a4dbfb73f
DWilson29 <deltadw8@gmail.com>
2026-03-15 21:19:34 -0700

    Add OwnAudio

---
+++ Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.Collections;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ConWerter.Models
+{
+    internal static class Player
+    {
+      static public void Beep(bool isLong, float volume)
+      {
+
+      }
+    }
+}
